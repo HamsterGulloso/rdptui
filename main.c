e50fc0c3fdbd4a901227400c6b2ec8527045a8da
HamsterGulloso <danielgullovasconcelos@gmail.com>
2026-02-23 19:05:24 -0300

    Very stupid storage system

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
+#include <assert.h>
+#include <errno.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
+#include <dirent.h>
 
 #define TB_IMPL
 #include "termbox2/termbox2.h"
 
 #define DYNAMIC_RES "+dynamic-resolution"
+
+#define PROGRAM_NAME "rdptui"
+#define SERVER_DIR "/.local/share/"  PROGRAM_NAME  "/"
+#define SERVER_FILE SERVER_DIR "saved_servers"
+
+char server_dir[255];
+char server_file[255];
 
 typedef struct {
     char* hostname;
@@ -48,6 +60,85 @@ void serverarray_remove(ServerArray* sa, size_t index) {
     for (int i = index; i < sa->size; i ++) {
         sa->s[i] = sa->s[i+1];
     }
+}
+
+int read_till_null(char* output_str, FILE* file) {
+    int fread_result;
+    char read_char;
+    size_t strsize = 0;
+
+    do {
+        fread_result = fread(&read_char, sizeof(char), 1, file);
+
+        if (fread_result != 1) return -1;
+        if (read_char == 0) break;
+
+        output_str[strsize++] = read_char;
+    } while (1);
+
+    output_str[strsize] = 0;
+    return strsize;
+}
+
+ServerArray* storage_read() {
+    int read_size;
+    char read_str[255];
+    Server aux;
+    ServerArray* sa;
+    FILE* storage = NULL;
+
+    sa = malloc(sizeof(ServerArray));
+    serverarray_init(sa);
+
+    storage = fopen(server_file, "rb");
+
+    if (storage == NULL) {
+        return sa;
+    }
+
+    do {
+        read_size = read_till_null(read_str, storage);
+        if(read_size == -1) break;
+        aux.hostname = malloc(sizeof(char)*read_size);
+        strcpy(aux.hostname, read_str);
+
+        read_size = read_till_null(read_str, storage);
+        if(read_size == -1) break;
+        aux.username = malloc(sizeof(char)*read_size);
+        strcpy(aux.username, read_str);
+
+        serverarray_append(sa, aux);
+    } while(1);
+
+    fclose(storage);
+    return sa;
+}
+
+int storage_save(ServerArray* sa) {
+    DIR* dir = opendir(server_dir);
+    FILE* file;
+
+    if (dir == NULL) {
+        mkdir(server_dir, 0777);
+    }
+
+    file = fopen(server_file, "w+b");
+
+    if (file == NULL) {
+        return 0;
+    }
+
+    for (int i = 0; i < sa->size; i ++) {
+        fwrite(sa->s[i].hostname, sizeof(char), strlen(sa->s[i].hostname), file);
+        fwrite("\0", sizeof(char), 1, file);
+
+        fwrite(sa->s[i].username, sizeof(char), strlen(sa->s[i].username), file);
+        fwrite("\0", sizeof(char), 1, file);
+    }
+
+    fclose(file);
+
+    return 1;
 }
 
 void position_texts(char** text, int num_texts, float x, float y, int selected_index) {
@@ -120,7 +211,7 @@ void fork_xfreerdp(char* hostname, char* username, char* password) {
 
         dup2(dev_null, STDOUT_FILENO);
         dup2(dev_null, STDERR_FILENO);
-        execl("/bin/xfreerdp", "xfreerdp", username_string, hostname_string, password_string, DYNAMIC_RES, NULL);
+        execl("/bin/xfreerdp", "/bin/xfreerdp", username_string, hostname_string, password_string, DYNAMIC_RES, NULL);
     }
 }
 
@@ -134,23 +225,31 @@ void update_items(char*** hostnames, char*** usernames, ServerArray* sa) {
     }
 }
 
-int main() {
+int main(int argc, char** argv) {
     int width;
     int height;
     struct tb_event e;
     int selected_option = 0;
-    ServerArray sa;
+    ServerArray* sa;
     char password[255];
     char* hostname;
     char* username;
     char** hostnames;
     char** usernames;
+    
+    if (getenv("HOME") == NULL) {
+        perror("AAAAAAA");
+        return 1;
+    }
 
-    serverarray_init(&sa);
+    sprintf(server_dir, "%s" SERVER_DIR, getenv("HOME"));
+    sprintf(server_file, "%s" SERVER_FILE, getenv("HOME"));
 
-    hostnames = malloc((sa.size+1)*sizeof(char*));
-    usernames = malloc((sa.size+1)*sizeof(char*));
-    update_items(&hostnames, &usernames, &sa);
+    sa = storage_read(); 
+
+    hostnames = malloc((sa->size+1)*sizeof(char*));
+    usernames = malloc((sa->size+1)*sizeof(char*));
+    update_items(&hostnames, &usernames, sa);
     hostnames[0] = "Hostnames:";
     usernames[0] = "Usernames:";
 
@@ -162,8 +261,8 @@ int main() {
 
         tb_clear();
 
-        position_texts(hostnames, sa.size + 1, 1./3., .5, selected_option + 1);
-        position_texts(usernames, sa.size + 1, 2./3., .5, selected_option + 1);
+        position_texts(hostnames, sa->size + 1, 1./3., .5, selected_option + 1);
+        position_texts(usernames, sa->size + 1, 2./3., .5, selected_option + 1);
         tb_present();
 
         tb_poll_event(&e);
@@ -186,28 +285,29 @@ int main() {
             if (!handle_input("Username:", username)) continue;
 
             serverarray_append(
-                &sa,
+                sa,
                 server_create(hostname, username)
             );
-            update_items(&hostnames, &usernames, &sa);
+            update_items(&hostnames, &usernames, sa);
+            storage_save(sa);
         }
 
         if (e.key == TB_KEY_ARROW_UP) {
             selected_option = selected_option - 1;
-            selected_option = selected_option < 0 ? sa.size - 1 : selected_option;
+            selected_option = selected_option < 0 ? sa->size - 1 : selected_option;
         }
 
         if (e.key == TB_KEY_ARROW_DOWN) {
-            selected_option = (selected_option + 1) % sa.size;
+            selected_option = (selected_option + 1) % sa->size;
         }
 
         if (e.key == TB_KEY_ENTER) {
-            if (sa.size == 0) continue;
+            if (sa->size == 0) continue;
             int password_entered = handle_input("Password:", password);
             if (password_entered) {
                 fork_xfreerdp(
-                    sa.s[selected_option].hostname,
-                    sa.s[selected_option].username,
+                    sa->s[selected_option].hostname,
+                    sa->s[selected_option].username,
                     password
                 );
             }
